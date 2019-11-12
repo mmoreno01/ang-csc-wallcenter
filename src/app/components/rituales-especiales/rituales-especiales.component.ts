f318bf4ef5021f0047241a9dba934dea8d9dad3a
MIGUEL ANGEL MORENO <you@example.com>
2019-11-12 17:13:43 -0600

    se creo vista rituales especiales

---
+++ src/app/components/rituales-especiales/rituales-especiales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rituales-especiales',
+  templateUrl: './rituales-especiales.component.html',
+  styles: []
+})
+export class RitualesEspecialesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
